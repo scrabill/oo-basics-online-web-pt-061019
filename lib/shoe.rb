3eaf5db9dd89a2e11647f6aafe2a29c7ea1123a9
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-10 22:14:42 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition # Properties of the Shoe class
+
+  def initialize(brand) # A new show must have a brand associated with it
+    @brand = brand
+  end
+
+  # def cobble
+  #   if self.condition == "old" # If the condition of the show is old...
+  #     self.condition = "new" # Change it to new
+  #   end
+  #   puts "Your shoe is as good as new!"
+  # end
+
+  def cobble
+    if self.condition != "new" # If the condition of the show is NOT "new" (it can be "old", "wonky" or anything else)...
+      self.condition = "new" # Then change it to new
+    end
+    puts "Your shoe is as good as new!"
+  end
+
+end
