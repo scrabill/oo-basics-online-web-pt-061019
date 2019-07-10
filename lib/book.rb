ed2dcc6d27318e021da690cc93080fdf3907056c
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-10 21:55:48 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,32 +1,37 @@
 class Book
+
+  attr_accessor :title, :author, :page_count, :genre
+
   def initialize(title) # This method gets triggered automatically when something else (creation of an object instance) happens
     @title = title
   end
 
-  def title
-    @title
-  end
+  # The below works, however it's much cleaner to use the attr_accessor macro on line 3 which creates a getter and setter method in one line!
 
-  def author=(author)
-    @author = author
-  end
-  def author
-    @author
-  end
-
-  def page_count=(num)
-    @page_count = num
-  end
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-  def genre
-    @genre
-  end
+  # def title
+  #   @title
+  # end
+  #
+  # def author=(author)
+  #  @author = author
+  # end
+  # def author
+  #   @author
+  # end
+  #
+  # def page_count=(num)
+  #   @page_count = num
+  # end
+  # def page_count
+  #   @page_count
+  # end
+  #
+  # def genre=(genre)
+  #   @genre = genre
+  # end
+  # def genre
+  #   @genre
+  # end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
